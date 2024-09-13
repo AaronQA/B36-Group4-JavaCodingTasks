33b3f07ccd55bdd28eadb38b7bafa1fd1a5583e2
Julia <yuliya_nc@yahoo.com>
2024-09-13 15:54:34 -0700

    OddOrEven code (Julia)

--- src/Julia/OddOrEven.java
+++ src/Julia/OddOrEven.java
@@ -8,12 +8,12 @@ public class OddOrEven {
   //Solution
 
     public static void main(String[] args) {
-        int array[]= {35};
+        int array[]= {5,6};
         for(int i=0;i<array.length;i++){
             if(array[i] % 2==0) {
-                System.out.println("Even number");
+                System.out.println("Even number: "+array[i]);
             }else{
-                System.out.println("Odd number");
+                System.out.println("Odd number: "+array[i]);
             }
         }
     }
