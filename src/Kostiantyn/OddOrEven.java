16f7bea87163e8e97d619313065a5433f2cb0dd1
thekidkk <klinchevk@gmail.com>
2024-09-17 17:06:53 -0600

    Created a file

--- src/Kostiantyn/OddOrEven.java
+++ src/Kostiantyn/OddOrEven.java
@@ -1,11 +1,9 @@
 package Kostiantyn;
 
 public class OddOrEven {
-    /*Write  a method which can identify given int number is even or odd
-    Output ex:
-    identify(5); ->"Odd"
-    identify(6); ->"Even"*/
-        //Solution
-    }
-
-
+       /*Write  a method which can identify given int number is even or odd
+ Output ex:
+ identify(5); ->"Odd"
+ identify(6); ->"Even"*/
+    //Solution: ?
+}
