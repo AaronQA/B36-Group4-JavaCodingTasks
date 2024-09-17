ec6d52d541151a390b9f214089d900a687280a93
thekidkk <klinchevk@gmail.com>
2024-09-17 17:24:23 -0600

    Solved the task.

--- src/Kostiantyn/OddOrEven.java
+++ src/Kostiantyn/OddOrEven.java
@@ -1,6 +1,25 @@
 package Kostiantyn;
 
 public class OddOrEven {
+    public static String identify (int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+
+    }
+
+    public static void main (String[] args) {
+        System.out.println(identify(5));
+        System.out.println(identify(6));
+    }
+
+
+
+
+
+
        /*Write  a method which can identify given int number is even or odd
  Output ex:
  identify(5); ->"Odd"
