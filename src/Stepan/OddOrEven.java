a55b916fa8cd28df4c52a1b5ef819b7ceaadf1e6
profe <grrrabe@gmail.com>
2024-09-18 10:36:30 -0700

    Task OddOrEven has been completed

--- src/Stepan/OddOrEven.java
+++ src/Stepan/OddOrEven.java
@@ -1,6 +1,25 @@
 package Stepan;
 
 public class OddOrEven {
+
+    public static void main(String[] args) {
+
+        identify(5);
+        identify(6);
+
+    }
+
+    public static void identify(int num){
+
+        if(num%2==0){ // Check if the num is even
+            System.out.println("Number: "+num+"\nType: Even");
+        }else{  // Check if the num is odd
+            System.out.println("Number: "+num+"\nType: Odd");
+        }
+
+    }
+
+
     /*Write  a method which can identify given int number is even or odd
     Output ex:
     identify(5); ->"Odd"
