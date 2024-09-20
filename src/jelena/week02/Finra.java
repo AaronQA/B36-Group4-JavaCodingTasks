95c322fb6152e73766a6f441e0d3f5e2599456d7
Jelena <jelenadobras1@gmail.com>
2024-09-20 12:33:06 -0700

    Added main method to test the function.

--- src/jelena/week02/Finra.java
+++ src/jelena/week02/Finra.java
@@ -1,10 +1,14 @@
 package jelena.week02;
 
+import javax.imageio.stream.ImageInputStream;
+
 public class Finra {
-    //Write a method which prints out the numbers from 1 to 30 but for numbers which are a multiple of 3,
-    //print "FIN" instead of the number and
-    //for numbers which are a multiple of 5, print "RA" instead of the number.
-    //for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
+
+
+    public static void main(String[] args) {
+        printFinra();  // Call the method to print FINRA numbers from 1 to 30.
+    }
+
 
 
     public static void printFinra() {
@@ -19,5 +23,9 @@ public class Finra {
                 System.out.println(i);
             }
         }
+
     }
 }
+
+
+
