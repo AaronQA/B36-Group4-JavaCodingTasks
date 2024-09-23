0ba684d2a0914c262b168f2dfb0f39dbe6e370e4
KostiantynKK <klinchevk@gmail.com>
2024-09-23 15:37:29 -0600

    Created a solution for Finra task

---
+++ src/Kostiantyn/Finra.java
@@ -0,0 +1,24 @@
+package Kostiantyn;
+
+public class Finra {
+
+
+        public static void printNumbers() {
+            for (int i = 1; i <= 30; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FINRA");
+                } else if (i % 3 == 0) {
+                    System.out.println("FIN");
+                } else if (i % 5 == 0) {
+                    System.out.println("RA");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+            printNumbers();
+        }
+    }
+
