fe2dfe00e118d198ff38ba3ed78ba75790e76898
Aaron <aaron@cybertekschool.com>
2024-10-02 23:08:43 +0200

    Q4 Published

---
+++ src/Questions/reverseNegativeNumber.java
@@ -0,0 +1,10 @@
+package Questions;
+
+public class reverseNegativeNumber {
+    //Write a return method that can reverse digits of a negative number and return it as int
+    //input: -35  output: -53
+
+
+
+}
+
