26e7d36fbe8b04ccceb170c1950d0c0539c0f695
Aaron <aaron@cybertekschool.com>
2024-11-04 18:49:21 +0100

    Q5 Published

---
+++ src/Questions/FrequencyofCharacters.java
@@ -0,0 +1,12 @@
+package Questions;
+
+public class FrequencyofCharacters {
+    /*Write a return method that can find the frequency of characters
+    Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2*/
+
+
+
+
+
+    }
+
