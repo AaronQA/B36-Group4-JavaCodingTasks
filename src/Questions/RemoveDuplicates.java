5e8fcb71d6d66a9046ee037ab79b0fd0d90d597f
Aaron <aaron@cybertekschool.com>
2024-11-12 17:01:52 +0100

    Q6 Published

---
+++ src/Questions/RemoveDuplicates.java
@@ -0,0 +1,8 @@
+package Questions;
+
+public class RemoveDuplicates {
+    /*Write a return method that can remove the duplicated values from String
+        Ex:  removeDup("AAABBBCCC")  ==> ABC*/
+
+
+}
