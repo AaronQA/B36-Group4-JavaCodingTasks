069a747e011964746d801844a069c9ba3ed5178d
Aaron <aaron@cybertekschool.com>
2024-12-09 17:39:12 +0100

    Q10 arrayFindMinimum Published

---
+++ src/Questions/arrayFindMinimum.java
@@ -0,0 +1,12 @@
+package Questions;
+
+
+public class arrayFindMinimum {
+    // Write a method that can find the minimum  number from an int Array
+    //Ex:  int[] arr = {99, 12, 23, 32, 44, 57, 6} ==> 6
+
+
+
+
+
+}
